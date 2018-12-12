27ab644871ee86c50a57e8f95a964d88175daf3d
Daria <dashasiliv@gmail.com>
2018-12-12 16:03:31 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pjc_semestralka
+//
+//  Created by Daria Silivonenko on 12/12/2018.
+//  Copyright © 2018 Daria Silivonenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
